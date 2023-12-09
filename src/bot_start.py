9ecbb47dea2921580d855e952e2e4b59b3b204b8
robin-douziech <robin.douziech@etu.esisar.grenoble-inp.fr>
2023-12-09 11:51:38 +0100

    debug

--- src/bot_start.py
+++ src/bot_start.py
@@ -276,9 +276,7 @@ async def clock() :
 	minutes = time.split(':')[1]
 
 	time = f"{(int(hours)+int(bot.vars['clock_hour_offset']))%24}:{minutes}"
-
-	bot.log(f"time: {time}")
-
+	
 	if (int(hours)+int(bot.vars['clock_hour_offset'])) > 23 :
 		day = str(int(day)+1)
 		if len(day) < 2 :
