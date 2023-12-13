3d5bff6d5e5f7a76b7fc5b11c1bbbe4435c5ad57
robin-douziech <robin.douziech@etu.esisar.grenoble-inp.fr>
2023-12-13 17:58:17 +0100

    polls

--- src/polls.py
+++ src/polls.py
@@ -44,8 +44,9 @@ async def poll_gamebot(ctx, *args, **kwargs) :
 				if str(args[1]) in bot.polls :
 					poll_id = str(args[1])
 					msg = f"Voici les résultats du sondage :\n"
-					for i in range(1, len(bot.polls[poll_id]["reactions"])+1) :
-						msg += f"**{i}**: {bot.polls[poll_id]['reactions'][i-1]}\n"
+					for i in range(1, len(bot.polls[poll_id]["reactions"])) :
+						msg += f"**{i}**: {bot.polls[poll_id]['reactions'][i-1]} / "
+					msg += f"**{len(bot.polls[poll_id]["reactions"])}**: {bot.polls[poll_id]['reactions'][len(bot.polls[poll_id]["reactions"])-1]}\n"
 					poll_results(poll_id)
 					file = discord.File(f'poll_{poll_id}.png')
 					await author.dm_channel.send(msg, file=file)
